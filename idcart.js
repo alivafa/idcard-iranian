07a48ed3f8c506d21f7583421b514fba43636bfa
ali vafaey <vafa62ali@gmail.com>
2021-04-05 18:25:09 +0430

    adding javascript code to javascript file

---
+++ idcart.js
@@ -0,0 +1,26 @@
+const butt=document.getElementById('butt');
+const sheba=document.getElementById('meli');
+butt.addEventListener('click' ,add);
+function add(e) {
+    e.preventDefault();
+    let y = sheba.value
+    console.log(y[9]);
+    let h =0;
+    let cod = 10;
+    for (let i = 0; i < 9; i++) {
+        h += y[i] * cod;
+        cod = cod - 1;
+    }
+    console.log(h);
+    h =h%11;
+    console.log(h);
+    if (y.length != 10) {
+        alert("your cod is short");
+    } else {
+        if (h == y[9] && h < 2 || y[9] == 11 - h) {
+            alert(" code is ok");
+        } else {
+            alert("code is false");
+        }
+    }
+}
